9b3e83a3d71589ba4f22ad810c8f54fd624cbb14
theuglygram <iamerheren@gmail.com>
2022-04-23 08:58:08 +0100

    updated

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import Home from "components/Pages/Home";
 import Movies from "components/Pages/Movies";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "components/Pages/Login";
+import ScrollToTop from "components/ScrollToTop";
 
 function App() {
   return (
     <>
       <Router>
+        <ScrollToTop />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/movies" element={<Movies />} />
